979c9cd5affa60b10d16762e8652a4b0f92e2854
seangnewman <seangnewman@msn.com>
2021-02-13 18:10:50 -0700

    Review of foreach in IL

---
+++ Examples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example1();
+        }
+
+        static void Example1()
+        {
+            var names = new string[] { "Tammy", "Cody", "Connor" };
+
+            var nameList = new List<String>();
+            nameList.Add("Tammy");
+            nameList.Add("Cody");
+            nameList.Add("Connor");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Found {name}");
+            }
+
+        }
+    }
+}
